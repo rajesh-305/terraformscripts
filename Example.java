6a233db5470b0e8948b3c980b49459136d6eb2b4
dataquaint <dataquaint@gmail.com>
2026-03-12 16:52:44 +0000

    second commit

--- Example.java
+++ Example.java
@@ -3,5 +3,7 @@ public class Example
 public static void main(String[] args)
 {
 System.out.println("Hello world");
+System.out.println("Hope you're all doing great amidst the war");
+System.out.println("Pray for the peace of the world");
 }
 }
